d3f21168c3c963a505e6d2ff8df70f5259b84bb0
MariaRacariu <90383391+MariaRacariu@users.noreply.github.com>
2022-05-09 12:48:31 +0200

    Creating all Post Types

--- wordpress/wp-content/themes/wcm-theme/functions.php
+++ wordpress/wp-content/themes/wcm-theme/functions.php
@@ -10,22 +10,80 @@ function setup_wcm_theme(){
 
 add_action('after_setup_theme', 'setup_wcm_theme');
 
-function create_posttype() {
-  
-    register_post_type( 'movies',
+function create_custom_posttypes() {
     // Custom Post Types Options
+    register_post_type( 'wcm_travel',
         array(
             'labels' => array(
-                'name' => __( 'wcm_travel' ),
+                'name' => __( 'Travel' ),
                 'singular_name' => __( 'Travel' )
             ),
             'public' => true,
             'has_archive' => true,
             'show_in_rest' => true,
-  
         )
     );
+    // Custom Post Types Options
+    register_post_type( 'travel_matches',
+        array(
+            'labels' => array(
+                'name' => __( 'Travel Matches' ),
+                'singular_name' => __( 'Matches' )
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'show_in_rest' => true,
+        )
+    );
+    // Custom Post Types Options
+    register_post_type( 'travel_cup',
+        array(
+            'labels' => array(
+                'name' => __( 'Travel Cup' ),
+                'singular_name' => __( 'Cup' )
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'show_in_rest' => true,
+        )
+    );
+    // Custom Post Types Options
+    register_post_type( 'travel_camp',
+        array(
+            'labels' => array(
+                'name' => __( 'Travel Camp' ),
+                'singular_name' => __( 'Camp' )
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'show_in_rest' => true,
+        )
+    );
+    // Custom Post Types Options
+    register_post_type( 'travel_soccer',
+        array(
+            'labels' => array(
+                'name' => __( 'Travel Soccer' ),
+                'singular_name' => __( 'Soccer' )
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'show_in_rest' => true,
+        )
+    );  
+    // Custom Post Types Options
+    register_post_type( 'netr_team',
+        array(
+            'labels' => array(
+                'name' => __( 'Team' ),
+                'singular_name' => __( 'Team' ),
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'show_in_rest' => true,
+        )
+    );  
 }
 // Hooking up our function to theme setup
-add_action( 'init', 'create_posttype' );
+add_action( 'init', 'create_custom_posttypes');
 ?>
